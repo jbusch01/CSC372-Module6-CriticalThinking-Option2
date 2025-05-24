54e33a0ebdb2f0a2d6ce3be344bcbd5d5a79165e
jbusch01 <jonrbusch@gmail.com>
2025-05-24 09:50:31 -0600

    Re-commit

---
+++ src/StudentList.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+class Students {
+    public static void main(String[] args) {
+        String name;
+        String address;
+        int rollno;
+        ArrayList<Students> students = new ArrayList<>();
+    }
+}
